7b355fcc5a1c1c27f588b678a8388dc34952c3d0
osxi <osxi@users.noreply.github.com>
2016-03-15 16:30:01 -0500

    bump ember-cli to 2.3.0

--- tests/unit/initializer-test.js
+++ tests/unit/initializer-test.js
@@ -1,23 +1,18 @@
-import getOwner from 'ember-getowner-polyfill';
 import Ember from 'ember';
 import initialize from 'ember-cli-segment/initializer';
 
-var owner, application;
+let application;
 
 module('segmentInitializer', {
   setup: function() {
     Ember.run(function() {
       application = Ember.Application.create();
-      owner = getOwner(application);
       application.deferReadiness();
     });
   }
 });
 
-// Replace this with your real tests.
 test('it works', function() {
-  initialize(owner, application);
-
-  // you would normally confirm the results of the initializer here
+  initialize(application);
   ok(true);
 });
